e32db766eb7769c201ae761882abd80a1bd203a5
iboka <yboka.dkz@gmail.com>
2012-08-26 13:14:53 +0300

    My first commit

---
+++ hardway1.py
@@ -0,0 +1,10 @@
+#ex 18
+def print_two(*args):
+	arg1, arg2 = args
+	print "arg1: %r, arg2: %r" % (arg1, arg2)
+
+def print_two_again(arg1, arg2):
+	print "arg1: %r, arg2: %r" % (arg1, arg2)
+
+print_two("a", "b")
+print_two_again("one", "two")
